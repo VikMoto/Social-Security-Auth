6a6e6e6735370c953519a472209e0ab136909b02
ig.basalt@gmail.com <ig.basalt@gmail.com>
2023-07-25 19:03:09 +0300

    issues SecurityFilterChainConfig JwtAuthenticationToken

--- src/main/java/com/chatico/authservice/service/UserAccountService.java
+++ src/main/java/com/chatico/authservice/service/UserAccountService.java
@@ -16,7 +16,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
